b1f871d40af0f90b3183daced45b85ae8b6a4747
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-03-20 15:50:28 -0500

    Modificacion de repositorio

--- src/main/java/com/api/pokeapi/repositories/PokemonRepository.java
+++ src/main/java/com/api/pokeapi/repositories/PokemonRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.pokeapi.models.Pokemon;
 
 public interface PokemonRepository extends JpaRepository<Pokemon, Integer>{
+
+    Pokemon findByNombre(String nombre);
     
 }
