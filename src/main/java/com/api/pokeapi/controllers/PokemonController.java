29d635a7df58ce647890e4cca94bbbc607d2ccc8
Jaip3r <jaidergustavoolmo@ufps.edu.co>
2024-04-01 15:50:26 -0500

    Configuración de metodos permitidos

--- src/main/java/com/api/pokeapi/controllers/PokemonController.java
+++ src/main/java/com/api/pokeapi/controllers/PokemonController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,7 +37,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/pokeapi/v1")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:5173", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class PokemonController {
 
     private final PokemonService pokemonService;
